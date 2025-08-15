ce8d0d5fee2e67e43b9a640176a778142deb1d1f
Lk Imran <ihkokil@gmail.com>
2025-08-15 23:28:23 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/default/animate.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/review.css') }}">
     <link rel="stylesheet" href="{{ asset('css/default/swiper-bundle.min.css') }}"> <!-- Swiper styles -->
     <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}"> <!-- Animation library -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
