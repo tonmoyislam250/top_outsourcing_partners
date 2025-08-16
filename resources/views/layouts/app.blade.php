576d553c3a5788d1f73ca9eac86ef9dfdc0632fe
Md Iqbal Haider Khan <ihkokil@gmail.com>
2025-08-16 09:23:19 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/default/animate.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/default/swiper-bundle.min.css') }}"> <!-- Swiper styles -->
-    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}"> <!-- Animation library -->
+    
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link rel="stylesheet" href="{{ asset('css/about.css') }}">
